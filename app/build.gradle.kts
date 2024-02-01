cd04254f329f2a9a3406f47fd41178fc93d2f5d7
techwithAasish <aasish.mahato101@gmail.com>
2024-02-01 06:48:30 -0500

    initial setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation ("androidx.datastore:datastore-preferences-rxjava2:1.0.0")
     implementation ("androidx.datastore:datastore-preferences-rxjava3:1.0.0")
+
 }
